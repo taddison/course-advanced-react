4ef7aa953ac91a27c9172bbff7b304a7bcb7521b
Tim Addison <git@tjaddison.com>
2019-12-04 19:45:39 -0500

    add item, mutations, query

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,18 @@
-const mutations = {};
+const mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO are they logged in?
+
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    }, info);
+
+    return item;
+  }
+
+};
+
+
 
 module.exports = mutations;
