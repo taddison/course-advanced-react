8d4781a863ade9c5bef97ff2e0070e7b46520d58
Tim Addison <git@tjaddison.com>
2019-12-05 19:39:04 -0500

    scaffold item page

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
+import styled from "styled-components";
+import Item from "./Item";
 
 const ALL_ITEMS_QUERY = gql`
   query ALL_ITEMS_QUERY {
@@ -15,11 +17,22 @@ const ALL_ITEMS_QUERY = gql`
   }
 `;
 
+const Center = styled.div`
+  text-align: center;
+`;
+
+const ItemsList = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+`;
+
 export default class Items extends Component {
   render() {
     return (
-      <div>
-        <p>Items</p>
+      <Center>
         <Query query={ALL_ITEMS_QUERY}>
           {({ data, error, loading }) => {
             if (loading) {
@@ -28,10 +41,16 @@ export default class Items extends Component {
             if (error) {
               return <div>Error: {error.message}</div>;
             }
-            return <div>I found {data.items.length} items</div>;
+            return (
+              <ItemsList>
+                {data.items.map(item => (
+                  <Item key={item.id} item={item} />
+                ))}
+              </ItemsList>
+            );
           }}
         </Query>
-      </div>
+      </Center>
     );
   }
 }
